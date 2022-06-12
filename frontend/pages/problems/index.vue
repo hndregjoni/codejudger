5035baa540d5babada2b81f91af4abfc0a6af9e7
niki <nspiro19@epoka.edu.al>
2022-06-12 20:42:14 +0200

    Added list of problems

--- frontend/pages/problems/index.vue
+++ frontend/pages/problems/index.vue
@@ -1,127 +1,129 @@
+
+
 <template>
-<v-row  justify="center" class="py-7">
-  <v-card style="width:50%" >
-    <v-card-title>
-      Problems
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table 
-      class="mytab"
-      :headers="headers"
-      :items="problems"
-      :search="search"
-    ></v-data-table>
-  </v-card>
+  <v-row justify="center" class="py-7">
+    <v-card style="width:55%">
+      <v-card-title>
+        Problems
+        <v-spacer></v-spacer>
+        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+      </v-card-title>
+      <v-row>
+        <v-select style="width:29%" class="pa-2 ml-3" label="Difficulty level" :items="['easy', 'medium', 'hard']"
+          v-model="search"></v-select>
+        <v-select style="width:29%" class="pa-2 "
+          :items="['Not-attempted', 'Attempted', 'Solved', 'Frozen', 'Disabled']" label="Status" v-model="search">
+        </v-select>
+        <v-select style="width:29%" class="pa-2 mr-3" label="Tags" :items="['Python', 'Java', 'Go', 'Clojure']"
+          v-model="search"></v-select>
+      </v-row>
+      <v-data-table class="mytab" :headers="headers" :items="filteredItems" :search="search" hide-details>
+      </v-data-table>
+    </v-card>
   </v-row>
 </template>
 
 <style>
-.mytab th{
-   background-color: rgba(74, 72, 199, 0.493);
-  
-}
+.mytab th {
+  background-color: rgba(74, 72, 199, 0.493);
 
+}
 </style>
 
 <script>
 
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'Title',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Difficulty lvl.', value: 'difficulty' },
-          { text: 'Date posted', value: 'date' },
-          { text: 'Status', value: 'status' },
-          { text: 'Tags', value: 'tag' },
-        ],
-        problems: [
-          {
-            name: 'Frozen Yogurt',
-            difficulty: 159,
-            date: 6.0,
-            status: 24,
-            tag: 4.0,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      search: '',
+
+      headers: [
+        {
+          text: 'Title',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Difficulty lvl.', value: 'difficulty' },
+        { difficultyLvl: '' },
+        { text: 'Date posted', value: 'date', dataType: "Date" },
+        { text: 'Status', value: 'status' },
+        { text: 'Tags', value: 'tag' },
+      ],
+      problems: [
+        {
+          name: 'b',
+          difficulty: 'hard',
+          date: new Date().toISOString().substring(0, 10),
+          status: ' Not-attempted',
+          tag: 'Python',
+        },
+        {
+          name: 'a',
+          difficulty: 'easy',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Attempted',
+          tag: 'Java',
+        },
+        {
+          name: 'c',
+          difficulty: 'medium',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Solved',
+          tag: 'Python',
+        },
+        {
+          name: 'e',
+          difficulty: ' medium',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Attempted',
+          tag: 'Java',
+        },
+        {
+          name: 'f',
+          difficulty: 'hard',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Solved',
+          tag: 'Python',
+        },
+        {
+          name: 'd',
+          difficulty: 'easy',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Solved',
+          tag: 'Java',
+        },
+        {
+          name: 'g',
+          difficulty: 'hard',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Attempted',
+          tag: 'Python',
+        },
+        {
+          name: 'j',
+          difficulty: 'easy',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Frozen',
+          tag: 'Go',
+        },
+        {
+          name: 'k',
+          difficulty: 'easy',
+          date: new Date().toISOString().substring(0, 10),
+          status: 'Frozen',
+          tag: 'Java',
+        },
+      ],
+    }
+  },
+  computed: {
+    filteredItems() {
+      return this.problems.filter((i) => {
+        return !this.difficultyLvl || (i.difficulty === this.difficultyLvl);
+      })
+    }
   }
+
+}
 </script>
