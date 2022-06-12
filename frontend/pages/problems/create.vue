548911e9ba692ea97c2c17d8326ff3d85162961c
besatb21 <bjacaj19@epoka.edu.al>
2022-06-13 00:51:14 +0200

    Initial problem setter page

---
+++ frontend/pages/problems/create.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <h1>Create problem</h1>
+    <v-row class="my-auto">
+      <v-col cols="2">
+        <v-text-field
+          v-model="firstname"
+          label="Slug"
+          required
+          outlined
+          dense
+        ></v-text-field>
+      </v-col>
+      <v-col cols="8">
+        <v-text-field
+          filled 
+          v-model="firstname"
+          label="Title"
+          required
+          outlined
+          dense
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="2">
+         <v-checkbox
+         v-model="checkbox"
+        label="Slug same as ID"
+        dense
+         ></v-checkbox>
+      </v-col>
+    </v-row>
+
+    <v-row class="ma-auto">
+      <v-textarea
+        outlined
+        name="input-7-4"
+        value="Write problem statement here . . ."
+      ></v-textarea>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-subheader><h2>Languages :</h2> </v-subheader>
+      <v-select :items="['Python', 'Java']" placeholder="Language"></v-select>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-expansion-panels inset>
+        <v-expansion-panel>
+          <v-expansion-panel-header>Constraint</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Constraints that will be read from backend
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+
+    <v-row class="ma-auto">
+      <v-col>
+        <v-btn depressed disabled> Upload testcase </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn depressed> Create </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- frontend/pages/problems/index.vue
+++ frontend/pages/problems/index.vue
@@ -1,127 +1,136 @@
 <template>
   <v-container fluid class="pa-6">
-    <v-card fill-width>
-      <v-card-title>
-        Problems
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table 
-        class="mytab"
-        :headers="headers"
-        :items="problems"
-        :search="search"
-      ></v-data-table>
-    </v-card>
+    <v-row>
+      <v-col justify="center">
+        <v-card fill-width>
+          <v-card-title>
+            Problems
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            class="mytab"
+            :headers="headers"
+            :items="problems"
+            :search="search"
+          ></v-data-table>
+        </v-card>
+      </v-col>
+      <v-col >
+        <v-row  justify="end">
+          <v-date-picker v-model="picker"></v-date-picker>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <style>
-.mytab th{
-   background-color: rgba(74, 72, 199, 0.493);
-  
+.mytab th {
+  background-color: rgba(74, 72, 199, 0.493);
 }
-
 </style>
 
 <script>
-
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'Title',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Difficulty lvl.', value: 'difficulty' },
-          { text: 'Date posted', value: 'date' },
-          { text: 'Status', value: 'status' },
-          { text: 'Tags', value: 'tag' },
-        ],
-        problems: [
-          {
-            name: 'Frozen Yogurt',
-            difficulty: 159,
-            date: 6.0,
-            status: 24,
-            tag: 4.0,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      
+      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      
+      search: "",
+      headers: [
+        {
+          text: "Title",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Difficulty lvl.", value: "difficulty" },
+        { text: "Date posted", value: "date" },
+        { text: "Status", value: "status" },
+        { text: "Tags", value: "tag" },
+      ],
+      problems: [
+        {
+          name: "Frozen Yogurt",
+          difficulty: 159,
+          date: 6.0,
+          status: 24,
+          tag: 4.0,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: "7%",
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: "8%",
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: "16%",
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: "0%",
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: "2%",
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: "45%",
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: "22%",
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: "6%",
+        },
+      ],
+    };
+  },
+};
 </script>
