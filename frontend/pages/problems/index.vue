2149955c1da0497c52ad0f8e887c288eec28ff79
niki <nspiro19@epoka.edu.al>
2022-06-14 22:35:51 +0200

    Changes to problem list

--- frontend/pages/problems/index.vue
+++ frontend/pages/problems/index.vue
@@ -1,32 +1,45 @@
-
-
 <template>
-  <v-row justify="center" class="py-7">
-    <v-card style="width:55%">
-      <v-card-title>
-        Problems
-        <v-spacer></v-spacer>
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
-      </v-card-title>
-      <v-row>
-        <v-select style="width:29%" class="pa-2 ml-3" label="Difficulty level" :items="['easy', 'medium', 'hard']"
-          v-model="search"></v-select>
-        <v-select style="width:29%" class="pa-2 "
-          :items="['Not-attempted', 'Attempted', 'Solved', 'Frozen', 'Disabled']" label="Status" v-model="search">
-        </v-select>
-        <v-select style="width:29%" class="pa-2 mr-3" label="Tags" :items="['Python', 'Java', 'Go', 'Clojure']"
-          v-model="search"></v-select>
-      </v-row>
-      <v-data-table class="mytab" :headers="headers" :items="filteredItems" :search="search" hide-details>
-      </v-data-table>
-    </v-card>
-  </v-row>
+  <v-container fluid class="pa-6">
+    <v-row class="justify-space-around">
+      <v-col justify="center">
+        <v-card fill-width>
+          <v-card-title>
+            Problems
+            <v-spacer></v-spacer>
+            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details>
+            </v-text-field>
+          </v-card-title>
+          <v-row>
+            <v-select style="width:29%" class="pa-2 ml-3" label="Difficulty level" :items="['easy', 'medium', 'hard']"
+              v-model="search"></v-select>
+            <v-select style="width:29%" class="pa-2 "
+              :items="['Not-attempted', 'Attempted', 'Solved', 'Frozen', 'Disabled']" label="Status" v-model="search">
+            </v-select>
+            <v-select style="width:29%" class="pa-2 mr-3" label="Tags" :items="['Python', 'Java', 'Go', 'Clojure']"
+              v-model="search"></v-select>
+          </v-row>
+          <v-data-table class="mytab" :headers="headers" :items="filteredItems" :search="search" hide-details>
+          </v-data-table>
+        </v-card>
+      </v-col>
+      <v-col cols="auto">
+        <v-card class="pa-2 mb-4">
+          <v-btn block outlined text color="black" class="primary" to="/problems/create" nuxt>
+            Create problem
+          </v-btn>
+        </v-card>
+        <v-card>
+
+          <v-date-picker v-model="picker"></v-date-picker>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style>
 .mytab th {
   background-color: rgba(74, 72, 199, 0.493);
-
 }
 </style>
 
@@ -35,6 +48,7 @@
 export default {
   data() {
     return {
+      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       search: '',
 
       headers: [
