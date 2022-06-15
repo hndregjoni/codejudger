159e9d3e9529019a07e7c4089e4b7f2c6eb9b7c6
niki <nspiro19@epoka.edu.al>
2022-06-15 21:08:18 +0200

    Added contest description page

---
+++ frontend/pages/contests/slug/index.vue
@@ -0,0 +1,53 @@
+<template >
+
+    <div class=" my-5">
+        <v-col cols="12" md="12">
+            <div :class="`rounded-br-xl`" class="py-5  text-center primary ">
+                <label class="white--text"> <b>Contest name </b> </label>
+            </div>
+        </v-col>
+        <v-col align="center" class="justify-center py-6">
+            <v-chip class="ma-1 primary"  text-color="white">
+                <v-icon left>
+                    mdi-label
+                </v-icon> 
+                Tag1
+            </v-chip>
+            <v-chip class="ma-1  primary"  text-color="white">
+                <v-icon left>
+                    mdi-label
+                </v-icon>
+                Tag2
+            </v-chip>
+            <v-chip class="ma-1  primary"  text-color="white">
+                <v-icon left>
+                    mdi-label
+                </v-icon>
+                Tag3
+            </v-chip>
+            <v-chip class="ma-1 primary" text-color="white">
+                <v-icon left>
+                    mdi-label
+                </v-icon>
+                Tag4
+            </v-chip>
+            <v-chip class="ma-1 primary" text-color="white">
+                <v-icon left>
+                    mdi-label
+                </v-icon>
+                Tag5
+            </v-chip>
+        </v-col>
+ 
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
