a4fa044fb17b05e14e546df6883af239d2867d59
niki <nspiro19@epoka.edu.al>
2022-06-15 19:10:00 +0200

    Changed design of problem description

--- frontend/pages/problems/slug/_slug/index.vue
+++ frontend/pages/problems/slug/_slug/index.vue
@@ -1,59 +1,58 @@
 <template >
-    <div class=" my-5" >
-      <div 
-        :class="`rounded-r-xl` "
-        class="py-4  text-center primary width:200px "
-        
-      >
-      <label class="white--text" > #Problem 1</label>
-      </div>
-        <v-spacer></v-spacer>
-        <v-row class="justify-center">
-            <v-chip class="ma-6 primary" label text-color="white">
+
+    <div class=" my-5">
+        <v-col cols="12" md="12">
+            <div :class="`rounded-r-xl`" class="py-5  text-center primary ">
+                <label class="white--text"> #Problem 1</label>
+            </div>
+        </v-col>
+        <v-col align="center" class="justify-center py-6">
+            <v-chip class="ma-1 primary"  text-color="white">
                 <v-icon left>
                     mdi-label
-                </v-icon>
+                </v-icon> 
                 Tag1
             </v-chip>
-                        <v-chip class="ma-6  primary"  label text-color="white">
+            <v-chip class="ma-1  primary"  text-color="white">
                 <v-icon left>
                     mdi-label
                 </v-icon>
                 Tag2
             </v-chip>
-                        <v-chip class="ma-6  primary" label text-color="white">
+            <v-chip class="ma-1  primary"  text-color="white">
                 <v-icon left>
                     mdi-label
                 </v-icon>
                 Tag3
             </v-chip>
-                        <v-chip class="ma-6 primary"  label text-color="white">
+            <v-chip class="ma-1 primary" text-color="white">
                 <v-icon left>
                     mdi-label
                 </v-icon>
                 Tag4
             </v-chip>
-                        <v-chip class="ma-6 primary"  label text-color="white">
+            <v-chip class="ma-1 primary" text-color="white">
                 <v-icon left>
                     mdi-label
                 </v-icon>
                 Tag5
             </v-chip>
-        </v-row>
-              <v-textarea
-        filled
-        outlined
-        readonly
-        value="Problem statement . . ."
-      ></v-textarea>
-      <v-btn class="ma-1 primary"  dense flat> Solve </v-btn>
+            
+                    <v-col  align="center" cols="12" md="7"> 
+            <v-textarea align="center" filled outlined readonly value="Problem statement . . ."></v-textarea>
+            <v-btn   class="ma-1 primary" dense flat> Solve </v-btn>
+         </v-col>
+        </v-col>
+ 
     </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+
+        }
     }
 }
 </script>
