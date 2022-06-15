3ce40277c8bbf242373d1ae1987912f2a5f8d0f8
Hysen Ndregjoni <hndregjoni@hotmail.com>
2022-06-15 08:54:28 +0200

    Contests endpoints

--- backend/app/app/api/api_v1/endpoints/contests.py
+++ backend/app/app/api/api_v1/endpoints/contests.py
@@ -8,3 +8,33 @@ router = APIRouter()
 def get_contests() -> Any:
     """ Get contests"""
     return []
+
+@router.post("/")
+def create_contest() -> Any:
+    """ Create a contest """
+
+@router.get("/id/{id}")
+def get_contest_by_id(id: int) -> Any:
+    """ Get a contest by its id """
+
+@router.get("/slug/{slug}")
+def get_contest_with_slug(slug: str) -> Any:
+    """ Get a contest by its slug """
+
+@router.get("/id/{id}/problems")
+def get_contest_problems_by_id(id: int) -> Any:
+    """ Get a contest's problems by its id"""
+    pass
+
+@router.get("/slug/{slug}/problems")
+def get_contest_problems_by_id(slug: str) -> Any:
+    """ Get a contest's problems by its id"""
+    pass
+
+@router.get("/id/{id}/standings")
+def get_contest_standings_by_id(id: int) -> Any:
+    """ Get a contest's standings by its id """
+
+@router.get("/slug/{slug}/standings")
+def get_contest_standings_by_id(slug: str) -> Any:
+    """ Get a contest's standings by its slug """
